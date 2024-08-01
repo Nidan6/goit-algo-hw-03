e81b9f4b9f11bac8f0c23f2a5c2a15ab6e2127a2
Nidan6 <108980863+Nidan6@users.noreply.github.com>
2024-08-01 20:17:13 +0300

    Update HM_1 (M_3).py

--- HM_1 (M_3).py
+++ HM_1 (M_3).py
@@ -2,28 +2,34 @@ from datetime import datetime
 import re
 
 
-def get_days_from_today(date):
+def get_days_from_today(date_str):
+    def correct_date(date_str):
+        date = re.match(r"(\d{2})-(\d{2})-(\d{4})", date_str)
+        if date:
+            return f"{date.group(3)}-{date.group(2)}-{date.group(1)}"
+        else:
+            date = re.match(r"(\d{2})-(\d{1})-(\d{4})", date_str)
+            return f"{date.group(3)}-{date.group(2)}-{date.group(1)}"
+        return date_str
+
+    try:
+        reforming_date = re.sub(r"[./\s\\]", "-", date_str)
+        date = datetime.strptime(reforming_date, "%Y-%m-%d").date()
+    except:
+        new_date = correct_date(reforming_date)
+        date = datetime.strptime(new_date, "%Y-%m-%d").date()
+
     today = datetime.today().date()
     new_day = date - today
     return new_day.days
 
 while True:
+    inp_date = input("Print your date: ")
     try:
-        inp_date = input("Print your date: ")
-        def correct_date (date_str):
-            date = re.match(r"(\d{2})-(\d{2})-(\d{4})", date_str)
-            if date == True:
-                return f"{date.group(3)}-{date.group(1)}-{date.group(2)}"
-            else:
-                date = re.match(r"(\d{2})-(\d{1})-(\d{4})", date_str)
-                return f"{date.group(3)}-{date.group(1)}-{date.group(2)}"
-            return date_str
-
-        reforming_date = re.sub(r"[./\s\\]", "-", inp_date)
-        new_date = correct_date(reforming_date)
-        date = datetime.strptime(new_date, "%Y-%m-%d").date()
+        days_difference = get_days_from_today(inp_date)
+        print(f"Days from today: {days_difference}")
         break
     except ValueError:
         print("Incorrect input, try again!")
 
-get_days_from_today(date)
+get_days_from_today(inp_date)
