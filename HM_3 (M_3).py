28b8dc3f19941935d044c1250f27e88d95520bce
Nidan6 <108980863+Nidan6@users.noreply.github.com>
2024-08-01 20:31:11 +0300

    Update HM_3 (M_3).py

--- HM_3 (M_3).py
+++ HM_3 (M_3).py
@@ -1,23 +1,15 @@
 import re
 
-phone_number = [
-"    +38(050)123-32-34",
-"     0503451234",
-"(050)8889900",
-"38050-111-22-22",
-"38050 111 22 11   "]
+phone_number = input(f"Add your phone number: ")
 
 
 def normalize_phone(phone_number):
-    new_numbers = []
-    for phone in phone_number:
-        clean_numbers = re.sub(r'\D', '', phone)
-        if not clean_numbers.startswith('+'):
-            if clean_numbers.startswith("380"):
-                new_numbers.append(clean_numbers)
-            else:
-                clean_numbers = "+38" + clean_numbers
-        new_numbers.append(clean_numbers)
-    return new_numbers
+    clean_numbers = re.sub(r'\D', '', phone_number)
+    if not clean_numbers.startswith('+'):
+        if clean_numbers.startswith("380"):
+                clean_numbers = "+" + clean_numbers
+        else:
+            clean_numbers = "+38" + clean_numbers
+    return clean_numbers
 
-normalize_phone(phone_number)
+print (normalize_phone(phone_number))
